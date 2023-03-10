1e1965976b15262e1e1844895440e5bb6b6d924c
Iman Salehi <cse.isalehi@gmail.com>
2023-03-10 12:43:35 +0330

    add basic util

---
+++ src/main/java/ir/jimsa/LovelyRecipes/shared/Constants.java
@@ -0,0 +1,5 @@
+package ir.jimsa.LovelyRecipes.shared;
+
+public class Constants {
+    public static final int PUBLIC_ID_LENGTH = 32;
+}
---
+++ src/main/java/ir/jimsa/LovelyRecipes/shared/Utils.java
@@ -0,0 +1,32 @@
+package ir.jimsa.LovelyRecipes.shared;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+@Component
+public class Utils {
+    Logger logger = LoggerFactory.getLogger(Utils.class);
+
+    private String generateRandomString() {
+        logger.info("GenerateRandomString() has called");
+
+        Random random = new SecureRandom();
+        String alphabet = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        StringBuilder returnValue = new StringBuilder();
+        int length = Constants.PUBLIC_ID_LENGTH;
+        for (int i = 0; i < length; i++) {
+            returnValue.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+        return returnValue.toString();
+    }
+
+    public String getPublicId() {
+        return generateRandomString();
+    }
+
+}
