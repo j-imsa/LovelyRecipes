82299d5a5fce74469a0b62c4fc84bc1b4cba5625
Iman Salehi <cse.isalehi@gmail.com>
2023-03-12 21:56:06 +0330

    add create integration test bad request

--- src/test/java/ir/jimsa/LovelyRecipes/recipes/controller/RecipeControllerTest.java
+++ src/test/java/ir/jimsa/LovelyRecipes/recipes/controller/RecipeControllerTest.java
@@ -2,12 +2,14 @@ package ir.jimsa.LovelyRecipes.recipes.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ir.jimsa.LovelyRecipes.config.exception.SystemServiceException;
 import ir.jimsa.LovelyRecipes.recipes.model.dto.RecipeDto;
 import ir.jimsa.LovelyRecipes.recipes.model.request.RecipeRequest;
 import ir.jimsa.LovelyRecipes.recipes.model.response.RecipeResponse;
 import ir.jimsa.LovelyRecipes.recipes.repository.entity.RecipeEntity;
 import ir.jimsa.LovelyRecipes.recipes.service.RecipeService;
 import ir.jimsa.LovelyRecipes.recipes.service.impl.RecipeServiceImpl;
+import ir.jimsa.LovelyRecipes.shared.ErrorMessages;
 import ir.jimsa.LovelyRecipes.shared.MyApiResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -77,6 +79,30 @@ class RecipeControllerTest {
         // Assert
         assertNotNull(response);
         assertTrue(response.isAction());
+        assertEquals(HttpStatus.CREATED.value(), mvcResult.getResponse().getStatus());
         assertEquals(2, data.get("consumers"));
+    }
+
+    @Test
+    @DisplayName("creat a new recipe with invalid inputs")
+    void testCreateNewRecipe_WhenInputIsInvalid_ShouldReturnException() throws Exception {
+        // Arrange
+        RecipeRequest recipeRequest = new RecipeRequest();
+        recipeRequest.setTitle("");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(path)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(new ObjectMapper().writeValueAsString(recipeRequest));
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        String responseBodyAsString = mvcResult.getResponse().getContentAsString();
+
+        // Assert
+        assertNotNull(responseBodyAsString);
+        assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+        assertEquals(responseBodyAsString, "");
     }
 }
