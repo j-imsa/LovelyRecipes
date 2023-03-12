5441030e7ab5538ee4e46d3e94de4f3014cd1b6d
Iman Salehi <cse.isalehi@gmail.com>
2023-03-12 20:24:36 +0330

    impl general util test

--- src/test/java/ir/jimsa/LovelyRecipes/shared/UtilsTest.java
+++ src/test/java/ir/jimsa/LovelyRecipes/shared/UtilsTest.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test the general Util class")
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UtilsTest {
-    private static Utils utils;
 
-    @BeforeAll
-    static void setup() {
-        utils = new Utils();
-    }
-
-    @Order(1)
-    @RepeatedTest(5)
+    @RepeatedTest(10)
     @DisplayName("testing publicId generator")
     void testGetPublicId_WhenInputAny_ShouldReturnString() {
-        assertNotNull(utils.getPublicId());
-        assertEquals(Constants.PUBLIC_ID_LENGTH, utils.getPublicId().length());
+        // Arrange (Given)
+        Utils utils = new Utils();
+
+        // Act (When)
+        String generatedPublicId = utils.getPublicId();
+
+        // Assert (Then)
+        assertNotNull(generatedPublicId, "getPublicId returned null");
+        assertEquals(Constants.PUBLIC_ID_LENGTH, generatedPublicId.length(), "getPublicId returned a bad-length string");
     }
 }
