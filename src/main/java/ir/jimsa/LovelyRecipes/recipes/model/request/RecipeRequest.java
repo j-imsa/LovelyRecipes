74a1b3b69641bec174de3db2c2d4586d649c7d8e
Iman Salehi <cse.isalehi@gmail.com>
2023-03-12 20:23:20 +0330

    add doc to request

--- src/main/java/ir/jimsa/LovelyRecipes/recipes/model/request/RecipeRequest.java
+++ src/main/java/ir/jimsa/LovelyRecipes/recipes/model/request/RecipeRequest.java
@@ -14,9 +14,14 @@ import java.util.List;
 @Getter
 @Schema(name = "Recipe", description = "Request Recipe Model")
 public class RecipeRequest {
+    @Schema(description = "The name of recipe", example = "Ghormeh Sabzi")
     private String title;
+    @Schema(description = "All ingredients that made this recipe", example = "[\"Apple\", \"Milk\", \"Sugar\"]")
     private List<String> ingredients;
+    @Schema(description = "How we can made it? steps, info and tutorials", example = "First, boil the water, then add some milk ...")
     private String instructions;
+    @Schema(description = "It means this recipe works or is enough for how many people?", example = "4")
     private int consumers;
+    @Schema(description = "Is this food good for vegetarians? true means Yes, It is good for them and No means Not at all", example = "true")
     private boolean vegetarian;
 }
